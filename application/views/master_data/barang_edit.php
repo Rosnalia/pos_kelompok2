bf9df3b64e573f2e6a7aaada4b4544d369fdc0dc
ipaa <ti.rifanuh@gmail.com>
2023-12-04 23:22:38 +0700

    view edit barang

--- application/views/master_data/barang_edit.php
+++ application/views/master_data/barang_edit.php
@@ -7,27 +7,28 @@
  	<!-- form start -->
  	<?php foreach ($barang as $brg) : ?>
  		<form action="<?php echo base_url() . 'barang/update/' . $brg->idBarang; ?>" method="post" enctype="multipart/form-data">
-		 <div class="form-group"></div>
+ 			<div class="card-body">
+ 				<div class="form-group">
  					<label>ID Barang</label>
  					<input type="text" class="form-control" name="idBarang" placeholder="ID Barang" value="<?php echo $brg->idBarang ?>">
  				</div>
- 				<div class="form-group"></div>
+ 				<div class="form-group">
  					<label>ID Kategori</label>
  					<input type="text" class="form-control" name="idKategori" placeholder="ID Kategori" value="<?php echo $brg->idKategori ?>">
  				</div>
- 				<div class="form-group"></div>
+ 				<div class="form-group">
  					<label>Nama Barang</label>
  					<input type="text" class="form-control" name="namaBarang" placeholder="Nama Barang" value="<?php echo $brg->namaBarang ?>">
  				</div>
- 				<div class="form-group"></div>
+ 				<div class="form-group">
  					<label>Harga Beli</label>
  					<input type="text" class="form-control" name="hargaBeli" placeholder="Harga Beli" value="<?php echo $brg->hargaBeli ?>">
  				</div>
- 				<div class="form-group"></div>
+ 				<div class="form-group">
  					<label>Harga Jual</label>
  					<input type="text" class="form-control" name="hargaJual" placeholder="Harga Jual" value="<?php echo $brg->hargaJual ?>">
  				</div>
-				 <div class="form-group"></div>
+ 				<div class="form-group">
  					<label>Stock</label>
  					<input type="text" class="form-control" name="stock" placeholder="Stock" value="<?php echo $brg->stock ?>">
  				</div>
