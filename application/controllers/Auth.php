b9dc5f8e4e1657783df0c415521f60e3cdc04693
Rosnalia Wahyu Agustin <rosnaliaaaw@gmail.com>
2023-12-11 07:10:58 +0700

    auth login

---
+++ application/controllers/Auth.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Model_user');
+    }
+	public function login() {
+		$this->form_validation->set_rules('namaUser', 'Namauser', 'required', ['required' => 'Username Wajib diisi']);
+		$this->form_validation->set_rules('password', 'Password', 'required', ['required' => 'password Wajib diisi']);
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('login');
+		} else {
+			$auth = $this->Model_user->cek_login();
+			if($auth == FALSE) {
+				$this->session->set_flashdata('pean', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+				Username atau Password anda salah!
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				</div>');
+				redirect('auth/login');
+			} else {
+				$this->session->set_userdata('namaUser', $auth->namaUser);
+				$this->session->set_userdata('levelUser', $auth->levelUser);
+
+				switch($auth->levelUser) {
+					case 1 : redirect('dashboard');
+								break;
+					case 2 : redirect('dashboard');
+								break;
+					default : break;
+				}
+			}
+			}
+		}
+
+	public function logout() {
+		$this->session->sess_destroy();
+		redirect('login');
+	}
+}
